c42e9f9a7eb1446fc6f9cde61dec119b1ca714f5
damirpie <damirdamir692@gmail.com>
2023-04-10 04:02:37 +0600

    11 'requests' 9:44

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,7 +9,13 @@ class RegisterController extends Controller
     public function index(){
         return view('registration.index');
     }
-    public function store() {
-        return "Запрос на регистрацию";
+    public function store(Request $request) {
+//        $data = $request->all();
+//        $data = $request->all('name', 'email');
+//        $data = $request->except('_token');   //Исключения
+
+        $name = $request->input('name');    //if value is empty return 'null'
+        $email = $request->email;               //with input() identical
+        dd($name,$email);
     }
 }
